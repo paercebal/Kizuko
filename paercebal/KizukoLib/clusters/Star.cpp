06017b3f73730f4af63ff93ae29d5becccfa5b60
paercebal <paercebal@gmail.com>
2017-11-13 23:56:25 +0100

    Making stars pretty, adding nebula brackground

--- paercebal/KizukoLib/clusters/Star.cpp
+++ paercebal/KizukoLib/clusters/Star.cpp
@@ -9,9 +9,31 @@
 namespace paercebal::KizukoLib::clusters
 {
 
+sf::Color getGradient(const sf::Color & lower, const sf::Color & higher, float gradient)
+{
+   return { static_cast<sf::Uint8>(lower.r + (higher.r - lower.r) * gradient), static_cast<sf::Uint8>(lower.g + (higher.g - lower.g) * gradient), static_cast<sf::Uint8>(lower.b + (higher.b - lower.b) * gradient), static_cast<sf::Uint8>(lower.a + (higher.a - lower.a) * gradient) };
+}
+
+void getGradientStar(sf::CircleShape & c, const sf::Color & lower, const sf::Color & higher, float gradient, const Star::Position & position, float size)
+{
+   const float radius = size - (0.5f * size * gradient);
+   c.setFillColor(getGradient(lower, higher, gradient));
+   c.setRadius(radius);
+   c.setPosition({ position.x - radius, position.y - radius });
+}
+
+void getStarHalo(sf::CircleShape & c, const sf::Color & color, sf::Uint8 transparency, const Star::Position & position, float size)
+{
+   const float radius = size;
+   c.setFillColor({ color.r, color.g, color.b, transparency });
+   c.setRadius(radius);
+   c.setPosition({ position.x - radius, position.y - radius });
+}
+
 Star::Star(const GlobalResources & globalResources, const std::string & name_, sf::Color color_, sf::Vector3f center_, float size_)
    : super(globalResources)
    , name{ name_ }
+   , shapes(8)
    , size{ size_ }
    , color{ color_ }
 {
@@ -27,9 +49,14 @@ void Star::createShapes2D()
    {
       Star::Position & position = positions[0];
 
-      this->shape.setFillColor(this->color);
-      this->shape.setRadius(this->size);
-      this->shape.setPosition({ position.x - this->size, position.y - this->size });
+      getStarHalo(this->shapes[0], this->color, 16, positions[0], this->size * 3.f);
+      getStarHalo(this->shapes[1], this->color, 32, positions[0], this->size * 2.0f);
+      getStarHalo(this->shapes[2], this->color, 64, positions[0], this->size * 1.5f);
+      getStarHalo(this->shapes[3], this->color, 128, positions[0], this->size * 1.25f);
+      getGradientStar(this->shapes[4], this->color, sf::Color::White, 0, positions[0], this->size);
+      getGradientStar(this->shapes[5], this->color, sf::Color::White, .25f, positions[0], this->size);
+      getGradientStar(this->shapes[6], this->color, sf::Color::White, .50f, positions[0], this->size);
+      getGradientStar(this->shapes[7], this->color, sf::Color::White, 1.0f, positions[0], this->size);
 
       this->nameLabel.setString(this->name);
       this->nameLabel.setFont(this->getGlobalResources().getFontScifi().font);
@@ -42,7 +69,11 @@ void Star::createShapes2D()
 
 void Star::drawInto(sf::RenderTarget & renderTarget) const
 {
-   renderTarget.draw(this->shape);
+   for (const auto & c : this->shapes)
+   {
+      renderTarget.draw(c);
+   }
+
    renderTarget.draw(this->nameLabel);
 }
 
